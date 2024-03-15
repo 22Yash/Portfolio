43dba6714fa50c2d8d780be7df2dddf5d5e50f5e
22Yash <yash.223234108@vcet.edu.in>
2024-03-15 18:33:43 +0530

    succesfully created hero section

--- src/components/Contact/Maincontact.jsx
+++ src/components/Contact/Maincontact.jsx
@@ -1,109 +1,17 @@
 import React, { useEffect, useRef } from "react";
 
-import Links from "./Links";
-import Contactform from "./Contactform";
-import IntoAnimation from "./IntoAnimation";
-import Navbar from "../Header/Navbar";
-import Hamburger from "../Header/Hamburger";
-
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger);
 
 
 function Maincontact() {
   
 
-  const ref= useRef(null);
-  useEffect(() => {
-    const el =ref.current;
-    gsap.fromTo(el,{
-      y:100,
-      opacity:1
-    },{
-      opacity:1,
-      y:-90,
-      duration:2,
-      scrollTrigger:{
-        trigger:el,
-        markers:true,
-        start:"top=90",
-        end:"bottom-=50",
-        toggleActions:"play none none reverse"
-      }
-    })
-
-
-  } ,[])
+  
 
 
 
   return (
     <>
-      <div
-        id="parent"
-        
-        className=" xl:w-full h-screen z-12 overflow-x-hidden"
-      >
-        <IntoAnimation />
-        <Navbar  />
-         <div id="hamburger">
-          <Hamburger />
-         </div>
-        
-
-        <div
-          id="contact"
-          className="bg-[#828E82] w-full xl:h-lvh 
-    grid grid-cols-1 
-    xl:grid xl:grid-cols-3 "
-        >
-          <div id="leftDiv" className=" h-screen col-span-1 xl:col-span-2  ">
-            <div
-              id="contactHeading"
-              className=" text-3xl flex flex-col 
-      xl:text-7xl xl:font-semibold sm:text-5xl  "
-            >
-              <h4
-                className=" mt-10 ml-16 
-            xl:mt-32 xl:ml-36"
-              >
-                Let's start a
-              </h4>
-              <h4 className="  mt-15 ml-5 xl:ml-36 ">project together</h4>
-            </div>
-            <div
-              id="divider"
-              className="border-2 border-black w-5/6  ml-10 
-                      mt-8 opacity-50 xl:opacity-100
-                      sm:w-4/4 sm:ml-12 sm:m-10 sm:mt-12
-                      xl:w-3/4 xl:ml-36 xl:m-10 xl:mt-10
-                      "
-            ></div>
-            <Contactform/>
-          </div>
-
-          <div id="rightdiv" className=" h-screen p-5 mt-22 xl:col-span-1">
-            <div
-              id="contactDetails "
-              className="  absolute mt-16  font-semibold p-10
-            sm:mt-72  sm:ml-12
-            xl:ml-0 xl:mt-64"
-            >
-              <h3 className="opacity-40 ">CONTACT DETAILS </h3>
-              <h3>yashadoke@gmial.com</h3>
-              <h3>+91 9324781464</h3>
-            </div>
-            <div id="socials" className=" flex xl:mt-64 mt-36 xl:col-span-1">
-              <Links  columnDirection={true}/>
-            </div>
-          </div>
-        </div>
-        <div id="lastDiv" className="w-full xl:h-3/4  bg-[#828E82]">
-
-        </div>
-      </div>
-
+      connect em
 
     </>
   );
--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -10,17 +10,21 @@ function Hero() {
   
   return (
     <>
-    <div id="hero" className='w-full h-screen bg-[#828E82]'>
-      <div id="heroMyName" className="xl:flex xl:justify-center xl:p-10 xl:gap-2 xl:text-5xl xl:font-semibold">
-        <h3>  👋myself , </h3>
-        <h3>Yash</h3>
-      </div>
-      <div id="myRole" className="xl:text-8xl 
-      xl:mt-10
-      xl:font-bold xl:flex xl:flex-col xl:justify-center xl:items-center">
-        <h1 className='xl:h-28 '>Front-end developer</h1>
-        <h2 className='xl:h-28 '>& UI-UX Designer</h2>
-      </div>
+    <div id="hero" className='w-full h-screen bg-[#d6d6d0]'>
+         <div id="heroHeading" className='
+         sm:ml-28
+         xl:ml-10 xl:tracking-wide xl:pt-20'>
+          <h1 className='
+          text-6xl font-bold  text-center  pt-20  
+          sm:text-8xl sm:font-bold sm:font-helvetica sm:text-left sm:ml-20
+          xl:text-9xl xl:font-bold xl:text-left xl:p-0 xl:ml-72 font-helvetica'>I'm Yash</h1>
+         </div>
+         <div id="heroRole" className='xl:tracking-wide'>
+          <h1 className='
+          text-6xl font-bold  text-center font-helvetica p-20 
+          sm:text-8xl sm:w-5/6 sm:text-center sm:flex sm:p-0 sm:ml-20
+          xl:text-9xl xl:ml-44 xl:w-3/4  xl:p-0 xl:text-center xl:font-bold '>Creative Frontend Developer.</h1>
+         </div>
       
       
 
