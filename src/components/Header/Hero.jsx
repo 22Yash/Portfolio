3fe3005f61d484052a3bfdd15d37a9f28acfb0c8
22Yash <yash.223234108@vcet.edu.in>
2024-07-25 20:18:17 +0530

    .

--- src/components/Header/Hero.jsx
+++ src/components/Header/Hero.jsx
@@ -23,270 +23,270 @@ function Hero() {
   const infoRef = useRef(null);
 
 
-    useEffect(() => {
-      const screenWidth = window.innerWidth;
+    // useEffect(() => {
+    //   const screenWidth = window.innerWidth;
   
-      if (screenWidth > 1024) {
-        // Animation for desktop/laptop screens
-        const tlDesktop = gsap.timeline();
+    //   if (screenWidth > 1024) {
+    //     // Animation for desktop/laptop screens
+    //     const tlDesktop = gsap.timeline();
 
 
 
        
   
         
-        tlDesktop.fromTo(
-          heroHeadingRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-          },
-          {
-            y: 10,
-            opacity: 1,
-            duration: 1,
-            repeat: 0,
-            delay: 6,
-          }
-        );
+    //     tlDesktop.fromTo(
+    //       heroHeadingRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //       },
+    //       {
+    //         y: 10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         repeat: 0,
+    //         delay: 6,
+    //       }
+    //     );
     
-        tlDesktop.fromTo(
-          roleHeadingOneRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlDesktop.fromTo(
+    //       roleHeadingOneRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlDesktop.fromTo(
-          roleHeadingOnePartRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlDesktop.fromTo(
+    //       roleHeadingOnePartRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlDesktop.fromTo(
-          roleHeadingTwoRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlDesktop.fromTo(
+    //       roleHeadingTwoRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlDesktop.fromTo(
-          infoRef.current,
-          {
-            opacity: 0,
-            stagger: 1,
-          },
-          {
-            opacity: 1,
-            stagger: 1,
-          }
-        );
+    //     tlDesktop.fromTo(
+    //       infoRef.current,
+    //       {
+    //         opacity: 0,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         opacity: 1,
+    //         stagger: 1,
+    //       }
+    //     );
         
-      } else if (screenWidth <= 1024 && screenWidth > 768) {
-        const tlTablet = gsap.timeline();
+    //   } else if (screenWidth <= 1024 && screenWidth > 768) {
+    //     const tlTablet = gsap.timeline();
   
-        tlTablet.fromTo(
-          heroHeadingRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-          },
-          {
-            y: 10,
-            opacity: 1,
-            duration: 1,
-            repeat: 0,
-            delay: 10,
-          }
-        );
+    //     tlTablet.fromTo(
+    //       heroHeadingRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //       },
+    //       {
+    //         y: 10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         repeat: 0,
+    //         delay: 10,
+    //       }
+    //     );
     
-        tlTablet.fromTo(
-          roleHeadingOneRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlTablet.fromTo(
+    //       roleHeadingOneRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlTablet.fromTo(
-          roleHeadingOnePartRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: 10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlTablet.fromTo(
+    //       roleHeadingOnePartRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: 10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlTablet.fromTo(
-          roleHeadingTwoRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlTablet.fromTo(
+    //       roleHeadingTwoRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlTablet.fromTo(
-          infoRef.current,
-          {
-            opacity: 0,
-            stagger: 1,
-          },
-          {
-            opacity: 1,
-            stagger: 1,
-          }
-        );
-      } else {
-        const tlMobile = gsap.timeline();
+    //     tlTablet.fromTo(
+    //       infoRef.current,
+    //       {
+    //         opacity: 0,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         opacity: 1,
+    //         stagger: 1,
+    //       }
+    //     );
+    //   } else {
+    //     const tlMobile = gsap.timeline();
 
   
-        tlMobile.fromTo(
-          heroHeadingRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-          },
-          {
-            y: 10,
-            opacity: 1,
-            duration: 1,
-            repeat: 0,
-            delay: 13,
-          }
-        );
+    //     tlMobile.fromTo(
+    //       heroHeadingRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //       },
+    //       {
+    //         y: 10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         repeat: 0,
+    //         delay: 13,
+    //       }
+    //     );
     
-        tlMobile.fromTo(
-          roleHeadingOneRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlMobile.fromTo(
+    //       roleHeadingOneRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlMobile.fromTo(
-          roleHeadingOnePartRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlMobile.fromTo(
+    //       roleHeadingOnePartRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlMobile.fromTo(
-          roleHeadingTwoRef.current,
-          {
-            y: 80,
-            opacity: 0,
-            duration: 1,
-            stagger: 1,
-          },
-          {
-            y: -10,
-            opacity: 1,
-            duration: 1,
-            stagger: 1,
-          }
-        );
+    //     tlMobile.fromTo(
+    //       roleHeadingTwoRef.current,
+    //       {
+    //         y: 80,
+    //         opacity: 0,
+    //         duration: 1,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y: -10,
+    //         opacity: 1,
+    //         duration: 1,
+    //         stagger: 1,
+    //       }
+    //     );
     
-        tlMobile.fromTo(
-          infoRef.current,
-          {
-            y:10,
-            opacity: 0,
-            stagger: 1,
-          },
-          {
-            y:-10,
-            opacity: 1,
-            stagger: 1,
-          }
-        );
+    //     tlMobile.fromTo(
+    //       infoRef.current,
+    //       {
+    //         y:10,
+    //         opacity: 0,
+    //         stagger: 1,
+    //       },
+    //       {
+    //         y:-10,
+    //         opacity: 1,
+    //         stagger: 1,
+    //       }
+    //     );
 
 
-      tlMobile.fromTo(
-            hamburgerRef.current,
-            {
-              y:10,
-              opacity: 0,
-              stagger: 1,
-            },
-            {
-              y:-10,
-              opacity: 1,
-              stagger: 1,
-            }
-          );
-      }
-    }, []);
+    //   tlMobile.fromTo(
+    //         hamburgerRef.current,
+    //         {
+    //           y:10,
+    //           opacity: 0,
+    //           stagger: 1,
+    //         },
+    //         {
+    //           y:-10,
+    //           opacity: 1,
+    //           stagger: 1,
+    //         }
+    //       );
+    //   }
+    // }, []);
 
     
 
@@ -320,7 +320,7 @@ function Hero() {
           <h1
             ref={heroHeadingRef}
             className="hero 
-          text-7xl font-bold  text-center  pt-20  selection:bg-[#6c665e] selection:text-[#3d3a36]
+          text-7xl font-bold  text-center -pt-[10px]   selection:bg-[#6c665e] selection:text-[#3d3a36]
           md:text-8xl md:font-bold md:font-helvetica md:text-left md:-ml-10
           xl:text-9xl xl:font-bold xl:text-left xl:p-0 xl:ml-72 font-helvetica"
           >
@@ -330,21 +330,21 @@ function Hero() {
         <div id="heroRole" className="hero xl:tracking-wide text-[#3b3834]">
           <h1
             className="
-          text-7xl font-bold  text-center font-helvetica p-20 flex flex-col selection:bg-[#6c665e] selection:text-[#3d3a36]
+          text-7xl font-bold  text-center font-helvetica -pt-[10px] flex flex-col selection:bg-[#6c665e] selection:text-[#3d3a36]
           md:text-8xl md:w-5/6 md:text-center md:flex md:p-0 md:ml-20
           xl:text-9xl xl:ml-52 xl:max-w-3/4   xl:flex xl:flex-col xl:p-0 xl:text-center xl:font-bold "
           >
             <div className="flex  flex-col xl:flex  xl:flex-row xl:gap-10 xl:w-3/4">
-              <div ref={roleHeadingOneRef} className="h1 mt-3 selection:bg-[#6c665e] selection:text-[#3d3a36]">
+              <div ref={roleHeadingOneRef} className="h1 mt-[40px] xl:mt-3 selection:bg-[#6c665e] selection:text-[#3d3a36]">
                 Creative
               </div>
-              <div ref={roleHeadingOnePartRef} className="h1 mt-4 selection:bg-[#6c665e] selection:text-[#3d3a36]">
+              <div ref={roleHeadingOnePartRef} className="h1 -pt-[10px] xl:mt-4 selection:bg-[#6c665e] selection:text-[#3d3a36]">
                 Frontend
               </div>
             </div>
             <div
               ref={roleHeadingTwoRef}
-              className="h1 mt-4 ml-4 md:ml-5 md:-mt-2 xl:-ml-2 selection:bg-[#6c665e] selection:text-[#3d3a36]"
+              className="h1 -pt-[10px] ml-4 md:ml-5 md:-mt-2 xl:-ml-2 selection:bg-[#6c665e] selection:text-[#3d3a36]"
             >
               Developer.
             </div>
@@ -353,7 +353,7 @@ function Hero() {
         <div
           id="info"
           className="hero
-        text-2md -mt-16 w-2/3 text-center ml-[100px] font-medium
+        text-2md -mt-[30px] w-2/3 text-center ml-[100px] font-medium
         md:mt-5 md:ml-32 
         xl:text-2xl xl:ml-96 xl:mt-10 xl:font-medium xl:w-2/5 xl:text-center text-[#6e6760]"
         >
--- src/components/Work/Experience.jsx
+++ src/components/Work/Experience.jsx
@@ -1,5 +1,6 @@
 import React ,{useEffect , useRef} from 'react'
-import gsap from 'gsap'
+import gsap from 'gsap';
+import { FaCalendarWeek } from "react-icons/fa";
 
 function Experience() {
   
@@ -7,47 +8,59 @@ function Experience() {
   return (
     <div className='resume  h-[800px] w-[700px]  sm:w-full xl:w-full bg-[#333333]' id='Resume'>
       <div id="title" className='text-white   font-helvetica  font-semibold
+      text-[70px] ml-[40px] pt-[20px]
       xl:text-[90px] xl:ml-[100px] xl:pt-[40px]
        '>
         <h3>Experience</h3>
         
       </div> 
       <div id="organisation" className=''>
-        <div id="first" className='xl:w-full xl:h-[130px] xl:flex xl:justify-between
+        <div id="first" className='
+        w-full h-[100px]  flex justify-between
+        xl:w-full xl:h-[130px] xl:flex xl:justify-between
         text-[#bdbeb0] xl:p-[10px]  font-helvetica font-medium xl:mt-[20px] xl:pl-[40px]' >
          <div id="name">
-         <h3 className='text-[50px]  ml-[50px] '>Junior web devloper intern</h3>
-          <h5 className='text-[20px] ml-[60px] '>Zummit Infolabs</h5>
+         
+         <h3 className=' text-[30px] flex  gap-[10px] xl:gap-[30px] pl-[40px] xl:text-[50px]  xl:-ml-[10px] '>         <FaCalendarWeek  size={`30px`} className='xl:mt-[20px]'/>
+Junior web devloper intern</h3>
+          <h5 className='text-[20px]  pl-[40px] xl:ml-[60px] '>Zummit Infolabs</h5>
           
          </div>
           
-          <div id="duration" className='xl:mr-[60px] font-helvetica'>
-            <h4 className='text-[20px] mt-[20px]'>July 2024 - October 2024</h4>
+          <div id="duration" className='xl:mr-[60px] mr-[20px] font-helvetica'>
+            <h4 className='text-[20px] mt-[10px] xl:mt-[20px]'>July 2024 - Oct 2024</h4>
             <h4 className='float-end'>Remote</h4>
 
           </div>
         </div>
 
-        <div id="border" className='border-b-[3px] border-[#bcbcaf] w-[90%] ml-[80px] mt-[10px]'></div>
+        <div id="border" className='border-b-[3px] border-[#bcbcaf] w-[90%] xl:ml-[80px] ml-[40px] mt-[10px]'></div>
 
 
 
         {/* Prodigy Infotech */}
-        <div id="second" className='xl:w-full xl:h-[130px] xl:flex xl:justify-between
+        <div id="second" className='
+         w-full h-[100px]  flex justify-between
+        xl:w-full xl:h-[130px] xl:flex xl:justify-between
         text-[#bdbeb0] xl:p-[10px]  font-helvetica font-medium xl:mt-[20px] xl:pl-[40px]' >
          <div id="name">
-         <h3 className='text-[50px]  ml-[50px]'>Intern at Prodigy Infotech</h3>
-         <h5 className='ml-[50px]'>Enhanced my Web Development skills through this Internship.</h5>
+         <h3 className='text-[30px] flex gap-[10px] xl:gap-[30px] pl-[40px] xl:text-[50px]  xl:-ml-[10px] '>
+         <FaCalendarWeek  size={`30px`} className='xl:mt-[20px]'/>
+          Intern at Prodigy Infotech</h3>
+         <h5 className='text-[20px] w-[430px] pl-[40px] xl:ml-[50px]'>Enhanced my Web Development skills through this Internship.</h5>
         
           
          </div>
           
           <div id="duration" className='xl:mr-[60px] font-helvetica'>
-            <h4 className='text-[20px] mt-[20px]'>December 2023</h4>
+            <h4 className='text-[20px] mt-[20px]'>Dec 2023</h4>
             <h4 className='float-end'>Remote</h4>
 
           </div>
         </div>
+
+        <div id="border" className='border-b-[3px] border-[#bcbcaf] w-[90%] xl:ml-[80px] ml-[40px] mt-[40px]'></div>
+
       </div>
 
      
