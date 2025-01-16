9f9e1642e5bf7b883586fcfb1f0adf0231ae9926
22Yash <yashadoke@gmail.com>
2025-01-16 20:27:10 +0530

    Fix Tailwind config for Vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 export default {
   content: [
     "./index.html",
@@ -9,15 +11,11 @@ export default {
       fontFamily: {
         sans: ['Montserrat', 'sans-serif'],
         helvetica: ['Helvetica'],
-         
       },
-      scrollbar: ['rounded']
-      
+      scrollbar: ['rounded'],
     },
-    
   },
   plugins: [
-    require('tailwind-scrollbar')
+    tailwindScrollbar,
   ],
-}
-
+};
